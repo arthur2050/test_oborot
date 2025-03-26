7abbce39936486edac1497880ac6f4493f1b4fb5
Arthur Patsanovskiy <arthur2050@mail.ru>
2025-03-26 23:55:30 +0300

    Created the Controllers.

---
+++ src/Controller/GardenController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tree\AppleTree;
+use App\Repository\AbstractFruitRepository;
+use App\Repository\AbstractTreeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GardenController extends AbstractController {
+
+	#[Route('/garden/index', name: 'garden.index', methods: ['GET'])]
+	public function index(
+		AbstractTreeRepository $treeRepository,
+		AbstractFruitRepository $fruitRepository
+	): Response {
+		// Количество фруктов каждого типа
+		$fruitCounts = $treeRepository->countFruitsForEachTreeType();
+
+		// Общий вес фруктов каждого типа
+		$fruitWeights = $treeRepository->countTotalWeightForEachTreeType();
+
+		// Самое тяжелое яблоко
+		$heaviestApple = $fruitRepository->findHeaviestFruitByTreeType('apple');
+
+		return $this->render('garden_index.html.twig', [
+			'fruitCounts' => $fruitCounts,
+			'fruitWeights' => $fruitWeights,
+			'heaviestApple' => $heaviestApple,
+		]);
+	}
+}
